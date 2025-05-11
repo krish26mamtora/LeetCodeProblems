8d098f1fd9b5a309963b0721a3c8e2012eeee42a
krish mamtora <krishmamtora26@gmail.com>
2025-05-11 19:34:56 +0530

    Three Consecutive Odds

---
+++ 11-05-2025.cpp
@@ -0,0 +1,29 @@
+class Solution
+{
+public:
+    bool threeConsecutiveOdds(vector<int> &arr)
+    {
+        if (arr.size() <= 2)
+        {
+            return false;
+        }
+        int c = 0;
+        for (int i = 0; i < arr.size(); i++)
+        {
+
+            if (arr[i] % 2)
+            {
+                c++;
+            }
+            else
+            {
+                c = 0;
+            }
+            if (c >= 3)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+};
